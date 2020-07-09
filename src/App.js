b12133e50fabcf517d90aad9d1170ebb28ca513d
robert Salazar <11rsahome@gmail.com>
2020-07-09 17:59:10 +0200

    Consulta Api recibiendo respuesta

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Form from "./components/Form.jsx";
 function App() {
   const [search, saveSearch] = useState("");
+  useEffect(() => {
+    // Creamos una funcion para consultar la api
+    const consultApi = async () => {
+      //validamos  la busqueda
+      if (search === "") return;
+      const imagePerPage = 20;
+      const key = "17403963-01695487f8e9b771a23983074";
+      //pasamos los parametros necesario
+      const url = `https://pixabay.com/api/?key=${key}&q=${search}&per_page=${imagePerPage}`;
+
+      const resp = await fetch(url);
+      const res = await resp.json();
+      console.log(res.hits);
+    };
+    //llamamos la funcion una vez consultada
+    consultApi();
+    //dependencia search
+  }, [search]);
   return (
     <div className="container ">
       <div className="jumbotron bg-info ">
